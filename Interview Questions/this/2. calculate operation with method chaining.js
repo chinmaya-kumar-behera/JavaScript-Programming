c3547565aed87447c1993ad1eebaa345a445024f
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2024-07-03 20:06:24 +0530

    Create 2. calculate operation with method chaining.js

---
+++ Interview Questions/this/2. calculate operation with method chaining.js
@@ -0,0 +1,34 @@
+
+const calc = {
+    value: 0,
+    
+    add: function(num) {
+        this.value += num;
+        return this;
+    },
+    
+    multiply: function(num) {
+        this.value *= num;
+        return this;
+    },
+    
+    subtract: function(num) {
+        this.value -= num;
+        return this;
+    },
+    
+    getResult: function() {
+        return this.value;
+    }
+};
+
+const res = calc.add(100).multiply(10).subtract(100).add(500).value;
+console.log(res);  // Output: 1400
+
+calc.value = 0; // Reset the calculator value
+const test1 = calc.add(10).subtract(5).multiply(3).add(2).value;
+console.log(test1); 
+
+calc.value = 0; // Reset the calculator value
+const test2 = calc.multiply(2).add(10).subtract(4).add(20).value;
+console.log(test2);  
