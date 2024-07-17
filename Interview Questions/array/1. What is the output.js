ba082e3a7f80215683dd1f640d9a2962ae213104
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2024-07-17 23:52:50 +0530

    Create 1. What is the output.js

---
+++ Interview Questions/array/1. What is the output.js
@@ -0,0 +1,22 @@
+
+// manipulating with the property of array length
+
+const arr = [10, 20, 30]
+console.log(typeof arr)
+arr.length = 0
+
+// console.log(arr);
+
+// console.log([] - []);
+// console.log([] * []);
+// console.log(typeof [] + []);
+// console.log([] / []);
+// console.log([] == new Array());
+
+
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+
+console.log(arr1 === arr2);
+
+
